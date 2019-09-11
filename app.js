e2281a7e8105fdd1764c0deedc8dd4213bd229d5
Jair Lizarraga <alizarra@mx1.ibm.com>
2019-09-11 12:24:17 -0500

    First commit, CSC Web page

---
+++ app.js
@@ -0,0 +1,61 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var demoRouter = require('./routes/demo');
+var badgesRouter = require('./routes/badges');
+var calendarioRouter = require('./routes/calendario');
+var tutorialesRouter = require('./routes/tutoriales');
+
+const http = require('http');
+const reload = require('reload');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/demo', demoRouter);
+app.use('/badges', badgesRouter);
+app.use('/calendario', calendarioRouter);
+app.use('/tutoriales', tutorialesRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
+
+var port = process.env.PORT || 3000
+
+const server = http.createServer(app);
+server.listen(port, function() {
+    console.log("To view your app, open this link in your browser: http://localhost:" + port);
+});
+reload(app);
